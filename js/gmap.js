bcb680f0c35eb5f37489d98f52ee978956964045
Ray Chang <raychang@pixnet.tw>
2015-07-23 17:11:46 +0800

    refactor object

--- js/gmap.js
+++ js/gmap.js
@@ -1,72 +1,120 @@
-var map;
-var myLatlng;
-var mapOptions;
-var geocoder;
-var marker;
-function initialize() {
-    myLatlng = new google.maps.LatLng(25.0633936, 121.520279);
-    mapOptions = {
-        zoom: 15,
-        center: myLatlng,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-    geocoder = new google.maps.Geocoder();
-    setArea();
-}
-google.maps.event.addDomListener(window, 'load', this.initialize);
-var panorama;
-var circleTimer;
-var jsonData;
-var placeArr = [];
-function setArea() {
-    $.ajax({
-        type: "GET",
-        url: "data.json",
-        dataType: "json",
-        async: true,
-        success: function(data) {
-            jsonData = data;
-            $.each(data, function(index, value){
-                var listhtml = '<a href="#"  class="place list-group-item" data-id="' + index  + '">' + value.Location + '</>';
-                $('.place-list > ul').append(listhtml);
-                //console.log('ret= ' + value.ret + ' lng= ' + value.lng);
-                placeArr.push(new google.maps.LatLng(value.ret, value.lng));
-                var populationOptions = {
-                    strokeColor: '#FF0000',
-                    strokeOpacity: 0.6,
-                    strokeWeight: 2,
-                    fillColor: '#FF0000',
-                    fillOpacity: 0.1,
-                    map: map,
-                    center: placeArr[index],
-                    radius: 100
-                };
-                cityCircle = new google.maps.Circle(populationOptions);
-                var infoWindow = new google.maps.InfoWindow({
-                    content: value.Location
-                });
-                google.maps.event.addListener(cityCircle, 'click', function(ev){
-                    //infowindow
-                    //infoWindow.setPosition(ev.latLng);
-                    //infoWindow.open(map);
-                    //test street view
-                    var panoramaOptions = {
-                        position: ev.latLng,
-                        pov: {
-                          heading: 0,
-                          pitch: 10
-                        }
-                    };
-                    panorama = new google.maps.StreetViewPanorama(document.getElementById('viewstreet'), panoramaOptions);
-                    map.setStreetView(panorama);
-                    clearTimeout(circleTimer);
-                    circleTimer = setTimeout(runCircle, 2000);
-                });
-            });
+var gmap = ({
+    map: '',
+    geocoder: '',
+    placeArr: [],
+    panorama: '',
+    circleTimer: '',
+    streetPov: {
+        heading: 0,
+        pitch: 10
+    },
+    el: {
+        $placeList: null,
+        $placeItem: null
+    },
+    initEl: function() {
+        this.el.$placeList = $('.place-list > ul');
+        this.el.$placeItem = $('.place');
+    },
+    bindEvent: function() {
+        this.el.$placeItem.click(this.bind(this, this.handleClick));
+    },
+    handleClick: function(e) {
+        var placeid = $(e.target).data('id');
+        this.map.setCenter(this.placeArr[placeid]);
+        this.setStreetView(this.placeArr[placeid]);
+    },
+    initPosition: function() {
+        var myLatlng = new google.maps.LatLng(25.0633936, 121.520279);
+        mapOptions = {
+            zoom: 15,
+            center: myLatlng,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        gmap.map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+        //var geocoder = new google.maps.Geocoder();
+    },
+    initData: function() {
+        this.initEl();
+        $.getJSON("data.json").done(
+            this.bind(this, function(ret) {
+                this.zipArr = ret;
+                $.each(ret,
+                    this.bind(this, function(index, value) {
+                        var listhtml = '<a href="#"  class="place list-group-item" data-id="' + index  + '">' + value.Location + '</>';
+                        this.el.$placeList.append(listhtml);
+                        var latLngObject = this.getLatLng(value.ret, value.lng);
+                        this.placeArr.push(latLngObject);
+                        this.plotCircle(latLngObject);
+
+                    })
+                );
+                this.initEl();
+                this.bindEvent();
+            })
+        );
+    },
+    getLatLng: function(ret, lng) {
+        return new google.maps.LatLng(ret, lng);
+    },
+    plotCircle: function(latLngObject, location) {
+        var populationOptions = {
+            strokeColor: '#FF0000',
+            strokeOpacity: 0.6,
+            strokeWeight: 2,
+            fillColor: '#FF0000',
+            fillOpacity: 0.1,
+            map: this.map,
+            center: latLngObject,
+            radius: 100
+        };
+        cityCircle = new google.maps.Circle(populationOptions);
+        var infoWindow = new google.maps.InfoWindow({
+            content: location
+        });
+        google.maps.event.addListener(cityCircle, 'click', this.bind(this, function(ev) {
+            //infowindow
+            //infoWindow.setPosition(ev.latLng);
+            //infoWindow.open(map);
+            //test street view
+            this.setStreetView(ev.latLng);
+        }));
+    },
+    setStreetView: function(latLng) {
+        var panoramaOptions = {
+            position: latLng,
+            pov: {
+              heading: 0,
+              pitch: 10
+            }
+        };
+        this.panorama = this.getStreetViewPanorama(panoramaOptions);
+        this.map.setStreetView(this.panorama);
+        clearTimeout(this.circleTimer);
+        this.circleTimer = setTimeout(this.bind(this, this.runCircle), 2000);
+    },
+    getStreetViewPanorama: function(option) {
+        return new google.maps.StreetViewPanorama(document.getElementById('viewstreet'), option);
+    },
+    runCircle: function() {
+        this.streetPov.heading += 45;
+        if (360 === this.streetPov.heading) {
+            this.streetPov.heading = 0;
         }
-    });
-}
+        this.panorama.setPov(this.streetPov);
+        this.circleTimer = setTimeout(this.bind(this, this.runCircle), 2000);
+    },
+    init: function() {
+        google.maps.event.addDomListener(window, 'load', this.initPosition);
+        //this.initData();
+    },
+    bind: function(obj, method) {
+        return function() {
+            return method.apply(obj, [].slice.call(arguments));
+        };
+    }
+});
+gmap.init();
 var around;
 function randMove() {
     around = panorama.getLinks();
@@ -74,30 +122,7 @@ function randMove() {
     console.log(randPosition);
     panorama.setPano(around[randPosition].pano);
 }
-var streetPov = {heading: 0, pitch: 10};
-function runCircle() {
-    streetPov.heading += 45;
-    if (360 === streetPov.heading) {
-        streetPov.heading = 0;
-    }
-    panorama.setPov(streetPov);
-    circleTimer = setTimeout(runCircle, 2000);
-}
 
-jQuery(function() {
-    $(document).on('click', '.place', function(e) {
-        var placeid = $(this).data('id');
-        map.setCenter(placeArr[placeid]);
-        var panoramaOptions = {
-            position: placeArr[placeid],
-            pov: {
-              heading: 0,
-              pitch: 10
-            }
-        };
-        panorama = new google.maps.StreetViewPanorama(document.getElementById('viewstreet'), panoramaOptions);
-        map.setStreetView(panorama);
-        clearTimeout(circleTimer);
-        circleTimer = setTimeout(runCircle, 2000);
-    });
+$(function() {
+    gmap.initData();
 });
